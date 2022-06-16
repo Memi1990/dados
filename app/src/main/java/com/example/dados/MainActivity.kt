3872a5081979a7a100be4de96a84ff98676e7fda
Kotlin <noemilopezpina@gmail.com>
2022-06-16 23:38:45 +0200

    Commit primeros arreglos

--- app/src/main/java/com/example/dados/MainActivity.kt
+++ app/src/main/java/com/example/dados/MainActivity.kt
@@ -10,27 +10,52 @@ import kotlin.random.nextInt
 
 class MainActivity : AppCompatActivity() {
     lateinit var b:ActivityMainBinding
-    val n1 = Random.nextInt(1..7)
-    val n2 = Random.nextInt(1..7)
+    var n1 = Random.nextInt(1..7)
+    var n2 = Random.nextInt(1..7)
 //    var dados = arrayOf(R.drawable.dice1, R.drawable.dice2, R.drawable.dice3, R.drawable.dice4, R.drawable.dice5, R.drawable.dice6)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         b =ActivityMainBinding.inflate(layoutInflater)
         setContentView(b.root)
-//        val d1 = (1..6).random()
-        b.imvDado1.setImageResource(roll(n1))
-        b.imvDado2.setImageResource(roll(n2))
-        b.btnRoll.text = if (n1>n2){
-        }
+
+    b.btnRoll.setOnClickListener {
+        val imgResource = when(n1){
+            1 -> R.drawable.dice1
+            2 -> R.drawable.dice2
+            3 -> R.drawable.dice3
+            4 -> R.drawable.dice4
+            5 -> R.drawable.dice5
+            else -> R.drawable.dice6 }
+        b.imvDado1.setImageResource(imgResource)
+
+        val imgResource2 = when(n2){
+            1 -> R.drawable.dice1
+            2 -> R.drawable.dice2
+            3 -> R.drawable.dice3
+            4 -> R.drawable.dice4
+            5 -> R.drawable.dice5
+            else -> R.drawable.dice6 }
+        b.imvDado2.setImageResource(imgResource2)
+        if (n1>n2){
+            b.tvResult.text = getText(R.string.winner1)
+        }else if (n1<n2){
+            b.tvResult.text = getText(R.string.winner2)
+        }else b.tvResult.text = "Empate"
+    }
+    b.btnNew.setOnClickListener {
+        n1 = Random.nextInt(1..7)
+        n2 = Random.nextInt(1..7)
+        b.tvResult.text = getText(R.string.result)
+    }
 
     }
-    fun roll(num:Int): Int {
-    val imgResource = when(num){
-        1 -> R.drawable.dice1
-        2 -> R.drawable.dice2
-        3 -> R.drawable.dice3
-        4 -> R.drawable.dice4
-        5 -> R.drawable.dice5
-        else -> R.drawable.dice6 }
-    }
+//    fun roll(num:Int) {
+//    val imgResource = when(num){
+//        1 -> R.drawable.dice1
+//        2 -> R.drawable.dice2
+//        3 -> R.drawable.dice3
+//        4 -> R.drawable.dice4
+//        5 -> R.drawable.dice5
+//        else -> R.drawable.dice6 }
+//    }
 }
